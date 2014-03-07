bfde2badd544febb228e0b161ea55e0191155531
biggor <bi990r@gmail.com>
2014-03-07 17:23:09 -0500

    poc 1

--- bikerally-dashboard/src/ca/biggor/bikerally/dashboard/Bikerally_checkinServlet.java
+++ bikerally-dashboard/src/ca/biggor/bikerally/dashboard/Bikerally_checkinServlet.java
@@ -1,9 +1,6 @@
 package ca.biggor.bikerally.dashboard;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.sql.Timestamp;
 import java.util.Date;
 
@@ -11,34 +8,74 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
 
 @SuppressWarnings("serial")
 public class Bikerally_checkinServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Bikerally - checkin");
-		resp.getWriter().println("-------------------");
-		resp.getWriter().println();
-		
-		resp.getWriter().print("artez id: ");
+		String firstName = null;
+		String lastName = null;
+		String fullName = null;
+		String participantLink = null;
+		String registrationDate = null;
+		String totalRaised = null;
 		String artezId = req.getParameter("id");
-		resp.getWriter().println(artezId);
-		
-		resp.getWriter().print("timestamp: ");
 		Date date = new Date();
-		resp.getWriter().println(new Timestamp(date.getTime()));
-		resp.getWriter().println();
-			
-		
-		URL url = new URL("http://my.e2rm.com//webgetservice/get.asmx/getRegistrant?registrantID=" + artezId + "&Source=&uniqueID=");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-		String line;
-		while ((line = reader.readLine()) != null) {
-			resp.getWriter().println(line);
+		Timestamp timestamp = new Timestamp(date.getTime());
+
+		DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+		DocumentBuilder b;
+		try {
+			b = f.newDocumentBuilder();
+			Document doc = b.parse("http://my.e2rm.com/webgetservice/get.asmx/getRegistrant?registrantID=" + artezId + "&Source=&uniqueID=");
+			doc.getDocumentElement().normalize();
+			firstName = doc.getElementsByTagName("firstName").item(0).getFirstChild().getNodeValue();
+			lastName = doc.getElementsByTagName("lastName").item(0).getChildNodes().item(0).getNodeValue();
+			participantLink = doc.getElementsByTagName("participantLink").item(0).getChildNodes().item(0).getNodeValue();
+			registrationDate = doc.getElementsByTagName("RegistrationDate").item(0).getChildNodes().item(0).getNodeValue();
+			totalRaised = doc.getElementsByTagName("totalRaised").item(0).getChildNodes().item(0).getNodeValue();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		resp.setContentType("text/plain");
+		if (firstName != null) {
+			fullName = firstName + " " + lastName;
+		} else {
+			fullName = "unknown user";
+		}
+		
+		resp.getWriter().println(fullName);
+		resp.getWriter().println("47.000, 39.123");
+		resp.getWriter().println(timestamp);
+		resp.getWriter().println("Thank you for checking-in!");
+		resp.getWriter().println();
+		resp.getWriter().println("Debug:");
+		resp.getWriter().println("Artez Id: " + artezId);
+		resp.getWriter().println("Timestamp: " + timestamp);
+		resp.getWriter().println("Geolocation: ");
+		resp.getWriter().println("firstName: " + firstName);
+		resp.getWriter().println("lastName: " + lastName);
+		resp.getWriter().println("participantLink: " + participantLink);
+		resp.getWriter().println("registrationDate: " + registrationDate);
+		resp.getWriter().println("totalRaised: " + totalRaised);
+
+		// URL url = new
+		// URL("http://my.e2rm.com/webgetservice/get.asmx/getRegistrant?registrantID="
+		// + artezId + "&Source=&uniqueID=");
+		// BufferedReader reader = new BufferedReader(new
+		// InputStreamReader(url.openStream()));
+		// String line;
+		// while ((line = reader.readLine()) != null) {
+		// resp.getWriter().println(line);
+		// }
+
 	}
 
 }
--- bikerally-dashboard/war/checkin.jsp
+++ bikerally-dashboard/war/checkin.jsp
@@ -21,33 +21,56 @@
 <title>Check-in</title>
 </head>
 <body>
-<p>Bikerally - checkin</p>
-<p>Artez id: ${param.id}</p>
 
 <%@ page import="java.util.Date" %>
 <%@ page import="java.sql.Timestamp" %>
 <%@ page import="java.net.URL" %>
 <%@ page import="java.io.InputStreamReader" %>
 <%@ page import="java.io.BufferedReader" %>
+<%@ page import="javax.xml.parsers.DocumentBuilder" %>
+<%@ page import="javax.xml.parsers.DocumentBuilderFactory" %>
+<%@ page import="org.w3c.dom.Document" %>
 
 <%
-	Date date = new Date();
-	Timestamp timestamp = new Timestamp(date.getTime());
+String firstName = null;
+String lastName = null;
+String fullName = null;
+String participantLink = null;
+String registrationDate = null;
+String totalRaised = null;
+Date date = new Date();
+Timestamp timestamp = new Timestamp(date.getTime());
+
+DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+DocumentBuilder b;
+try {
+	b = f.newDocumentBuilder();
+	Document doc = b.parse("http://my.e2rm.com/webgetservice/get.asmx/getRegistrant?registrantID=" + request.getParameter("id") + "&Source=&uniqueID=");
+	doc.getDocumentElement().normalize();
+	firstName = doc.getElementsByTagName("firstName").item(0).getFirstChild().getNodeValue();
+	lastName = doc.getElementsByTagName("lastName").item(0).getChildNodes().item(0).getNodeValue();
+	participantLink = doc.getElementsByTagName("participantLink").item(0).getChildNodes().item(0).getNodeValue();
+	registrationDate = doc.getElementsByTagName("RegistrationDate").item(0).getChildNodes().item(0).getNodeValue();
+	totalRaised = doc.getElementsByTagName("totalRaised").item(0).getChildNodes().item(0).getNodeValue();
+} catch (Exception e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+if (firstName != null) {
+	fullName = firstName + " " + lastName;
+} else {
+	fullName = "unknown user";
+}
  %>
-<p>Timestamp: <%= timestamp %></p>
-<p><span>Geolocation: </span><span id="geo"></span></p>
+	<center>
+		<div>
+			<img style="width: 20%;" alt="" src="check.png">
+		</div>
+		<h1><%= fullName %></h1>
+		<h3 id="geo" />
+		<h3><%= timestamp %></h3>
+	</center>
 
-<div>
-<%
-		URL url = new URL("http://my.e2rm.com//webgetservice/get.asmx/getRegistrant?registrantID=" + request.getParameter("id") + "&Source=&uniqueID=");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-		String line;
-		while ((line = reader.readLine()) != null) {
-%>
-<%= line %>
-<%
-		}
-%>
-</div>
 </body>
 </html>
