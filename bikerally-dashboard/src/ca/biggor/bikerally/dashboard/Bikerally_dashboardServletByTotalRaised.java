6e4d7d1c9ce80df3b90aea93baba776a6aff9fae
Victor Scaletchi <bi990r@gmail.ocm>
2014-07-03 16:28:37 -0400

    sortByTotalRised

---
+++ bikerally-dashboard/src/ca/biggor/bikerally/dashboard/Bikerally_dashboardServletByTotalRaised.java
@@ -0,0 +1,61 @@
+package ca.biggor.bikerally.dashboard;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.SortDirection;
+
+@SuppressWarnings("serial")
+public class Bikerally_dashboardServletByTotalRaised extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		resp.setContentType("text/plain");
+		resp.getWriter().println("BikeRally - Dashboard");
+
+		PreparedQuery riders = getParticipants("124639");
+		PreparedQuery crew = getParticipants("125616");
+
+		resp.getWriter().println();
+		resp.getWriter().println("Riders: " + riders.countEntities(FetchOptions.Builder.withDefaults()) + ", Crew: " + crew.countEntities(FetchOptions.Builder.withDefaults()));
+		resp.getWriter().println();
+		resp.getWriter().println("Rider list");
+		printParticipants(riders, resp);
+		resp.getWriter().println();
+		resp.getWriter().println("Crew list");
+		printParticipants(crew, resp);
+	}
+
+	private PreparedQuery getParticipants(String eventId) {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query(eventId).addSort("id", SortDirection.ASCENDING).setFilter(new FilterPredicate("status", FilterOperator.EQUAL, "active"));
+		return datastore.prepare(q);
+	}
+
+	private void printParticipants(PreparedQuery participants, HttpServletResponse resp) throws IOException {
+		resp.getWriter().println("i,rised,name");
+		int index = 0;
+		for (Entity participant : participants.asIterable()) {
+			String id = (String) participant.getProperty("id");
+			String firstName = (String) participant.getProperty("firstName");
+			String lastName = (String) participant.getProperty("lastName");
+			String riderNumber = (String) participant.getProperty("riderNumber");
+			String totalRaised = (String) participant.getProperty("totalRaised");
+			resp.getWriter().println(String.format("%3s", index += 1) + "," + String.format("%10s", totalRaised) + "," + firstName + " " + lastName);
+		}
+	}
+
+}
